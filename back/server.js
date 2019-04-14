8eba05f6b796c17e1e907f2f9671388ba085171e
anarcoqzn <anarco.nascimento@ccc.ufcg.edu.br>
2019-04-14 19:41:47 -0300

    reestruturação dos imports

--- back/server.js
+++ back/server.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Iniciando aplicacao
 const app = express();
+
+// Iniciando e conectando o DB
+mongoose.connect('mongodb://localhost:27017/contare',{useNewUrlParser: true});
+
 app.listen(3001);
