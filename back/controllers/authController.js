1c684515353e352f095e7033fa45db829e7fef53
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2019-04-27 23:19:38 -0300

    refazendo refatoramento de Marcus

--- back/controllers/authController.js
+++ back/controllers/authController.js
@@ -18,14 +18,14 @@ module.exports = {
         try{
             // se o email ja for usado o usuario n pode se cadastrar 
             if(await User.findOne({ email }))
-                return res.status(400).send({ error: "User already exists"});
+                return res.status(400).send({ error: "Já existe um usuário cadastrado com este email"});
             
             const user = await User.create(req.body);
             user.password = undefined;
-            return res.send({ user ,  token: generateToken({id: user.id}),});
+            return res.send({ user ,  token: generateToken({id: user.id}),  sucess: "Cadastrado com sucesso!"});
     
         }catch (err){
-            return res.status(400).send({ error: "Registration failure"});
+            return res.status(400).send({ error: "Ocorreu um erro inesperado!"});
         }
     },
     
@@ -34,10 +34,10 @@ module.exports = {
          const user = await User.findOne( { email } ).select("+password");
     
          if(!user)
-            return res.status(400).send({ error: "User not found" });
+            return res.status(400).send({ error: "Usuário não encontrado" });
     
          if(!await bcrypt.compare(password, user.password))
-            return res.status(400).send({ error: "Invalid password" });
+            return res.status(400).send({ error: "Senha inválida" });
     
          user.password = undefined;
          const token = jwt.sign({ id: user.id }, authConfig.secret, {
