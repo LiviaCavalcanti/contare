6a14160c910e824b61102fa212d0897ddcf1fd92
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2019-04-28 11:41:00 -0300

    excluindo expense group e alterando user em rotas

--- back/route.js
+++ back/route.js
@@ -5,25 +5,16 @@ const ExpenseController = require("./controllers/ExpenseController");
 const authController = require("./controllers/authController")
 
 // Usuario ------------
-router.get("/user/:id", UserController.show);
-router.post("/signup", UserController.store);
-router.put("/user/:id", UserController.update);
-router.delete("/user/:id", UserController.delete);
+router.get("/user", UserController.show);
+router.put("/user", UserController.update);
 //---------
 
 // Tarefas
-router.get("/user/:userID/expenses", ExpenseController.showALL);
-router.get("/user/:userID/expenses/:expID", ExpenseController.showONE);
+router.get("/user/:userID/expenses", ExpenseController.index);
+router.get("/user/:userID/expenses/:expID", ExpenseController.show);
 router.post("/user/:userID/expenses", ExpenseController.store);
 router.put("/user/:userID/expenses/:expID", ExpenseController.update);
 router.delete("/user/:userID/expenses/:expID", ExpenseController.delete);
-
-// Grupos
-router.get("/user/:userID/groups", ExpenseController.showALL);
-router.get("/user/:userID/groups/:grpID", ExpenseController.showONE);
-router.post("/user/:userID/groups", ExpenseController.store);
-router.put("/user/:userID/expenses/:grpID", ExpenseController.update);
-router.delete("/user/:userID/expenses/:grpID", ExpenseController.delete);
 
 router.post("/register", authController.register);
 router.post("/authenticate", authController.authenticate);
