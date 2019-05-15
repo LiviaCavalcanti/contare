d353e58e6c81015fc8cea92edf04c250b298ae6f
Anarco Nascimento <anarco.nascimento@ccc.ufcg.edu.br>
2019-05-15 13:40:36 -0300

    Criação da entidade Convite (Invitation)

---
+++ back/models/Invitation.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const InviteSchema = new mongoose.Schema({
+
+    from:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+      },
+
+    to:{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+
+    expirationDate:{
+      type: Date
+    },
+    
+    expense:{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Expense'
+    }
+});
