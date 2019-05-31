47617f16b1f51e5e51c50e008f899d94e748fd66
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2019-05-30 23:03:57 -0300

    começando teste de despesa com convite

--- back/tests/test.js
+++ back/tests/test.js
@@ -6,7 +6,7 @@ const expect    = chai.expect
 const chaiHttp = require('chai-http')
 const server = require('../server')
 const should = chai.should();
-
+const requestAgent = require('supertest')
 chai.use(chaiHttp)
 
 
@@ -35,3 +35,51 @@ describe('Testing registration routes', function(){
     });
 
 });
+
+describe('creating invites', function() {
+
+    beforeEach( ( done ) => {
+        
+        var url = '/register'
+        chai.request('http://localhost:8080/contare')
+            .post(url)
+            .send({'name':"Teste1", 'email':"teste1@mail.com", 'password':"teste1"})
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
+            .end((err, res) => {
+                should.exist(res);
+                res.should.have.status(200)
+                done()
+            })
+    })
+
+    it('login', (done) => {
+        var serverAgent = requestAgent.agent('http://localhost:8080/contare')
+        var user = {
+            name: 'Teste1',
+            email:"teste1@mail.com", 
+            password:"teste1"
+        }
+        var user_token;
+
+        serverAgent
+            .post('/authenticate')
+            .send(user)
+            .end( (err, res) => {
+                if (err) return done(err)
+                should.exist(res);
+                user_token = res.body.token
+                res.should.have.status(200)
+
+                chai.request('http://localhost:8080/contare')
+                    .post('/user/expenses')
+                    .send({ title: 'ExpenseTest', listEmail: [{ payValue: 0 }] })
+                    .set('Content-Type', 'application/json')
+                    .set('x-access-token', user_token)
+                    .end((err, resnumber2) => {
+                        should.exist(resnumber2);
+                        done()
+                    })
+            })
+    })
+})
