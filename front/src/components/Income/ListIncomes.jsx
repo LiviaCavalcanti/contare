557776b0181fad6ed82db47967e420cff27cae17
Gabriel Fernandes <gabrielfernnd@gmail.com>
2019-10-21 00:52:49 -0300

    Opcoes para ordenar renda

--- front/src/components/Income/ListIncomes.jsx
+++ front/src/components/Income/ListIncomes.jsx
@@ -1,22 +1,16 @@
 import {StatsCard} from 'components/StatsCard/StatsCard.jsx'
-import {Grid, Col} from 'react-bootstrap'
+import {Grid, Col, FormGroup, FormControl, ControlLabel, Row} from 'react-bootstrap'
 import React, {useState, useEffect} from 'react'
 import {getIncomes} from '../../services/income'
 import Income from './Income'
 import {daysDiff, weeksDiff, monthsDiff, yearsDiff} from '../../utils/date'
 import '../../assets/css/custom.css'
 
-import { initializeConnection } from 'services/ConnectionService';
-
 export default function ListIncomes(props) {
     const [incomes, setIncomes] = useState([])
     const [incomeModals, setIncomeModals] = useState([])
-
-    var socket = initializeConnection();
-
-    socket.on("updateincome", function () {
-        props.setUpdate(true);
-    }.bind(this))
+    const [cachedIncomes, setCachedIncomes] = useState([])
+    const [sorting, setSorting] = useState('Data de Criação')
 
     useEffect(() => {
         if (props.update) {
@@ -24,7 +18,7 @@ export default function ListIncomes(props) {
             let totalIncome = 0
 
             getIncomes().then(resp => {
-                setIncomes(resp)
+                setCachedIncomes(resp)
 
                 setIncomeModals(resp.map(income => {
                     let receivedDate = new Date(income.receivedOn)
@@ -50,6 +44,30 @@ export default function ListIncomes(props) {
         }
     })
 
+    useEffect(() => {
+        sortIncomes()
+    }, [cachedIncomes, sorting])
+
+    function sortIncomes() {
+        if (sorting == 'Data de Criação') setIncomes(cachedIncomes.slice())
+        else if (sorting == 'Título') setIncomes(cachedIncomes.slice().sort((inc1, inc2) => {
+            if (inc1.title.toLowerCase() < inc2.title.toLowerCase()) return -1
+            return 1
+        }))
+        else if (sorting == 'Valor') setIncomes(cachedIncomes.slice().sort((inc1, inc2) => {
+            if (inc1.value > inc2.value) return -1
+            return 1
+        }))
+        else if (sorting == 'Data de Recebimento') setIncomes(cachedIncomes.slice().sort((inc1, inc2) => {
+            if (new Date(inc1.receivedOn) > new Date(inc2.receivedOn)) return -1
+            return 1
+        }))
+        else if (sorting == 'Tipo de Recorrencia') setIncomes(cachedIncomes.slice().sort((inc1, inc2) => {
+            if (inc1.periodicity > inc2.periodicity) return -1
+            return 1
+        }))
+    }
+
     function showModal(i) {
         let incModals = incomeModals.slice()
         incModals[i] = true
@@ -58,6 +76,20 @@ export default function ListIncomes(props) {
 
     return (
         <Grid fluid>
+            <Row>
+                <Col lg={3} sm={4} xs={6}>
+                    <FormGroup>
+                        <ControlLabel>Ordenar as rendas por</ControlLabel>
+                        <FormControl componentClass="select" value={sorting} onChange={val => setSorting(val.target.value)}>
+                            <option>Data de Criação</option>
+                            <option>Título</option>
+                            <option>Valor</option>
+                            <option>Data de Recebimento</option>
+                            <option>Tipo de Recorrencia</option>
+                        </FormControl>
+                    </FormGroup>
+                </Col>
+            </Row>
             {incomes.map((income, i) =>
                 <Col lg={4} sm={6} key={income._id}>
                     <StatsCard bigIcon={<i className="pe-7s-server text-warning" />}
