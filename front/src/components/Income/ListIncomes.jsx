a6baa90cce7c276989e740cd02491a849e9ee047
Gabriel Fernandes <gabrielfernnd@gmail.com>
2019-10-25 20:54:12 -0300

    Lista de rendas paginada

--- front/src/components/Income/ListIncomes.jsx
+++ front/src/components/Income/ListIncomes.jsx
@@ -1,5 +1,5 @@
 import {StatsCard} from 'components/StatsCard/StatsCard.jsx'
-import {Grid, Col, FormGroup, FormControl, ControlLabel, Row} from 'react-bootstrap'
+import {Grid, Col, FormGroup, FormControl, ControlLabel, Row, Pager} from 'react-bootstrap'
 import React, {useState, useEffect} from 'react'
 import {getIncomes} from '../../services/income'
 import Income from './Income'
@@ -11,6 +11,10 @@ export default function ListIncomes(props) {
     const [incomeModals, setIncomeModals] = useState([])
     const [cachedIncomes, setCachedIncomes] = useState([])
     const [sorting, setSorting] = useState('Data de Criação')
+    const [pageIndex, setPageIndex] = useState(0)
+    const [pageIncomes, setPageIncomes] = useState([])
+
+    const elemsPerPage = 12
 
     useEffect(() => {
         if (props.update) {
@@ -19,6 +23,9 @@ export default function ListIncomes(props) {
 
             getIncomes().then(resp => {
                 setCachedIncomes(resp)
+
+                if (elemsPerPage * pageIndex >= resp.length && pageIndex > 0)
+                    setPageIndex(pageIndex - 1)
 
                 setIncomeModals(resp.map(income => {
                     let receivedDate = new Date(income.receivedOn)
@@ -48,8 +55,12 @@ export default function ListIncomes(props) {
         sortIncomes()
     }, [cachedIncomes, sorting])
 
+    useEffect(() => {
+        setPageIncomes(incomes.slice(elemsPerPage * pageIndex, elemsPerPage * pageIndex + elemsPerPage))
+    }, [incomes])
+
     function sortIncomes() {
-        if (sorting == 'Data de Criação') setIncomes(cachedIncomes.slice())
+        if (sorting == 'Data de Criação') setIncomes(cachedIncomes.slice().reverse())
         else if (sorting == 'Título') setIncomes(cachedIncomes.slice().sort((inc1, inc2) => {
             if (inc1.title.toLowerCase() < inc2.title.toLowerCase()) return -1
             return 1
@@ -74,33 +85,59 @@ export default function ListIncomes(props) {
         setIncomeModals(incModals)
     }
 
+    function previous() {
+        setPageIndex(val => {
+            if (val > 0) val -= 1
+            setPageIncomes(incomes.slice(elemsPerPage * val, elemsPerPage * val + elemsPerPage))
+            return val
+        })
+    }
+
+    function next() {
+        setPageIndex(val => {
+            if (elemsPerPage * (val + 1) < incomes.length) val += 1
+            setPageIncomes(incomes.slice(elemsPerPage * val, elemsPerPage * val + elemsPerPage))
+            return val
+        })
+    }
+
     return (
-        <Grid fluid>
-            <Row>
-                <Col lg={3} sm={4} xs={6}>
-                    <FormGroup>
-                        <ControlLabel>Ordenar as rendas por</ControlLabel>
-                        <FormControl componentClass="select" value={sorting} onChange={val => setSorting(val.target.value)}>
-                            <option>Data de Criação</option>
-                            <option>Título</option>
-                            <option>Valor</option>
-                            <option>Data de Recebimento</option>
-                            <option>Tipo de Recorrencia</option>
-                        </FormControl>
-                    </FormGroup>
-                </Col>
-            </Row>
-            {incomes.map((income, i) =>
-                <Col lg={4} sm={6} key={income._id}>
-                    <StatsCard bigIcon={<i className="pe-7s-server text-warning" />}
-                        statsText={income.title}
-                        statsValue={"R$ " + income.value}
-                        statsIcon={<i className="fa fa-edit clickable" onClick={() => showModal(i)} />}
-                        statsIconText={<span className="clickable" onClick={() => showModal(i)}>Editar renda</span>}
-                    />
-                    <Income income={income} i={i} incomeModals={incomeModals} setIncomeModals={setIncomeModals} setUpdate={props.setUpdate} />
-                </Col>
-            )}
-        </Grid>
+        <>
+            <Grid fluid>
+                <Row>
+                    <Col lg={3} sm={4} xs={6}>
+                        <FormGroup>
+                            <ControlLabel>Ordenar as rendas por</ControlLabel>
+                            <FormControl componentClass="select" value={sorting} onChange={val => setSorting(val.target.value)}>
+                                <option>Data de Criação</option>
+                                <option>Título</option>
+                                <option>Valor</option>
+                                <option>Data de Recebimento</option>
+                                <option>Tipo de Recorrencia</option>
+                            </FormControl>
+                        </FormGroup>
+                    </Col>
+                </Row>
+                {pageIncomes.map((income, i) =>
+                    <Col lg={4} sm={6} key={income._id}>
+                        <StatsCard bigIcon={<i className="pe-7s-server text-warning" />}
+                            statsText={income.title}
+                            statsValue={"R$ " + income.value}
+                            statsIcon={<i className="fa fa-edit clickable" onClick={() => showModal(i)} />}
+                            statsIconText={<span className="clickable" onClick={() => showModal(i)}>Editar renda</span>}
+                        />
+                        <Income income={income} i={i} incomeModals={incomeModals} setIncomeModals={setIncomeModals} setUpdate={props.setUpdate} />
+                    </Col>
+                )}
+            </Grid>
+            <Pager>
+                <Pager.Item className={pageIndex <= 0 ? 'hidden' : ''} previous onClick={previous}>
+                    &larr; Anterior
+                </Pager.Item>
+                <Pager.Item className={elemsPerPage * (pageIndex + 1) >= incomes.length ? 'hidden' : ''} next onClick={next}>
+                    Próximo &rarr;
+                </Pager.Item>
+            </Pager>
+        </>
     )
 }
