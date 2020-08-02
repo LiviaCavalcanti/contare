f84a2fd30e950f6dd265f27dfa6b6fd9f6ad4967
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2020-08-01 22:33:53 -0300

    componente de pagina de erro adicionado

---
+++ front/src/components/page404Component/page404Component.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './page404Component.css'
+class Page404Component extends Component {
+  
+  render() {
+    return (
+      <div>
+        <h1>Oh oh...</h1>
+
+<section class="error-container">
+  <span class="four"><span class="screen-reader-text">4</span></span>
+  <span class="zero"><span class="screen-reader-text">0</span></span>
+  <span class="four"><span class="screen-reader-text">4</span></span>
+</section>
+<p class="zoom-area"> Infelizmente a página que você procura não pôde ser encontrada. Ela pode estar temporariamente indisponível, ter sido movida ou não existir mais. </p>
+<p class="zoom-area">Verifique se a URL que você procurou tem algum erro e tente novamente.</p>
+
+      </div>
+      
+    );
+  }
+}
+
+export default Page404Component;
