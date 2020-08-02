a97e81249f909c60666ec9698f5b0417b3332db1
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2020-08-01 22:41:09 -0300

    navbar extraida como componente

---
+++ front/src/components/HomeNavbar/HomeNavbar.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import logo from "assets/img/contare_logo.png";
+import './HomeNavbar.css'
+
+class HomeNavbar extends Component {
+    constructor(props) {
+        super(props)
+
+    }
+
+    redirect = (path) => {
+        this.props.history.push(path)
+    }
+
+    render(){
+    return (
+        <div class="homepage-lead-navbar">
+        <div class="homepage-lead-navbar-content">
+            <img src={logo} alt="logo_image" />
+            <span><a href="/" onClick={() => this.redirect('/')}>CONTARE</a></span>
+            <div class="homepage-lead-navbar-content-nav">
+                <span><a href="/register" onClick={() => this.redirect('/register')}>Registrar</a></span>
+                <span><a href="/login" onClick={() => this.redirect('/login')}>Login</a></span>
+            </div>
+        </div>
+    </div>
+    )}
+}
+
+export default HomeNavbar;
