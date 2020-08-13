a27d0691bbaec6471cfded4fb3523b73b32bcef1
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2020-08-13 12:56:01 -0300

    checkbox adicionado

--- front/src/views/exportBankExtractComponent/ListExtract.jsx
+++ front/src/views/exportBankExtractComponent/ListExtract.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import {Modal, Button} from 'react-bootstrap'
-import { Table } from 'react-bootstrap';
+import { Table, FormGroup, FormControl, InputGroup } from 'react-bootstrap';
+import "./ListExtract.css"
 
 export default class ListExtract extends React.Component {
 
     constructor(props) {
         super(props);
+        this.handleSubmit=this.handleSubmit.bind(this)
         this.state = {
           data: this.props.extractObjects
         }
     }
 
     handleSubmit(){
-      // fechar o modal de baixo
+      
       // notifySucess("Foram criadas " + objValues["createdExpenses"] + " despesas e " + objValues["createdIncomes"]+ " rendas!")
-      this.props.handleClose()
+      this.props.onHide()
     }
 
     render() {
@@ -41,22 +43,38 @@ export default class ListExtract extends React.Component {
                 <Modal.Title>Receitas e Despesas</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-            
-            <Table striped bordered hover>
-            {/* <thead>
-              <tr>
-                <th style={{ width: "25%" }}>Nome</th>
-                <th style={{ width: "25%" }}>Email</th>
-                <th >Valor</th>
-                <th className="acao" style={{ width: "20%" }}>Status</th>
-                <th className="acao" style={{ width: "20%" }}>Status Convite</th>
-              </tr>
-            </thead> */}
+       
+          <Table striped bordered hover>
+           <thead>
+            <tr>
+              <th style={{ width: "1%" }}></th>
+              <th style={{ width: "25%" }}>Título</th>
+              <th >Valor</th>
+              <th className="acao" style={{ width: "15%" }}>Tipo</th>
+              <th className="acao" style={{ width: "35%" }}>Descrição</th>
+            </tr>
+          </thead>
             <tbody>
+              
             {
                 this.props.extractObjects.map((obj, i) => {
                   return (
-                    <tr key={`objects-${i}`}>
+                    <tr key={`objects-${i}`}  className="mb-3">
+                      
+                      <td>
+                       
+                       <form>
+                       <FormGroup>
+    <InputGroup>
+      
+        <input type="checkbox" />
+      
+      
+    </InputGroup>
+  </FormGroup>
+
+                       </form>
+                      </td>
                       <td >
                         {obj.title}
                       </td>
@@ -75,10 +93,10 @@ export default class ListExtract extends React.Component {
                 })
               }
             </tbody>
-            </Table>
+            </Table> 
             </Modal.Body>
             <Modal.Footer>
-            <Button onClick={this.props.handleClose}>Fechar</Button>
+            <Button onClick={this.props.onHide}>Fechar</Button>
               <Button onClick={this.handleSubmit}  bsStyle="primary">Exportar</Button>
             </Modal.Footer>
         </Modal>
