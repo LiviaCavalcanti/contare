5c041a2ff0d7cc7772d85db14d75713893e99060
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2020-09-07 10:22:46 -0300

    definindo handleSubmit e handleCheckbox

--- front/src/views/exportBankExtractComponent/ListExtract.jsx
+++ front/src/views/exportBankExtractComponent/ListExtract.jsx
@@ -2,23 +2,51 @@ import React from 'react'
 import {Modal, Button} from 'react-bootstrap'
 import { Table, FormGroup, FormControl, InputGroup } from 'react-bootstrap';
 import "./ListExtract.css"
+import {addExpenses} from "../../services/expenseService"
+import {notifyFailure, notifySucess} from "../../services/notifyService"
 
 export default class ListExtract extends React.Component {
 
     constructor(props) {
         super(props);
         this.handleSubmit=this.handleSubmit.bind(this)
+        this.handleCheckbox=this.handleCheckbox.bind(this)
         this.state = {
-          data: this.props.extractObjects
+          data: this.props.extractObjects,
+          selectedData: [],
         }
     }
 
-    handleSubmit(){
-      
-      // notifySucess("Foram criadas " + objValues["createdExpenses"] + " despesas e " + objValues["createdIncomes"]+ " rendas!")
+    async handleSubmit(){
+      var createdExpenses = 0
+      console.log(this.state.selectedData)
+      for (var obj in this.state.selectedData) {
+        console.log(obj)
+        var retorno = await addExpenses(obj, function(){})
+        if (retorno) {
+          createdExpenses += 1
+        }
+      }
+        
+      notifySucess("Foram criadas " + createdExpenses + " despesas")
       this.props.onHide()
     }
 
+    handleCheckbox(event, obj1) {
+      console.log(event)
+      const {value, checked} = event.target; // get id and checked status
+      var selectedData = this.state.selectedData.slice();
+      console.log(obj1)
+      // Add to an array if checked and remove - if uncheck
+      if (checked) {
+        selectedData.push(obj1);
+      } else {
+        selectedData = selectedData.filter(obj => obj !== obj1);
+      }
+      console.log(selectedData)
+      this.setState({ selectedData: selectedData });
+    }
+  
     render() {
       let partners = this.props && this.props.extractObjects.length > 0 ?
       this.props.extractObjects.map( (obj, i)=>
