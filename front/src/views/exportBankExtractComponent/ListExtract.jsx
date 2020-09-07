81d1942be4710330ec6b3e1f26e8eed842e2aa26
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2020-09-07 10:35:19 -0300

    ajustes no codigo

--- front/src/views/exportBankExtractComponent/ListExtract.jsx
+++ front/src/views/exportBankExtractComponent/ListExtract.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import {Modal, Button} from 'react-bootstrap'
-import { Table, FormGroup, FormControl, InputGroup } from 'react-bootstrap';
+import { Table} from 'react-bootstrap';
 import "./ListExtract.css"
 import {addExpenses} from "../../services/expenseService"
-import {notifyFailure, notifySucess} from "../../services/notifyService"
+import {notifySucess} from "../../services/notifyService"
 
 export default class ListExtract extends React.Component {
 
@@ -27,44 +27,30 @@ export default class ListExtract extends React.Component {
           createdExpenses += 1
         }
       }
-        
-      notifySucess("Foram criadas " + createdExpenses + " despesas")
+      if (createdExpenses > 0) {
+        notifySucess("Foram criadas " + createdExpenses + " despesas")
+      }
+      else {
+        notifySucess("Nenhuma despesa foi criada")
+      }
       this.props.onHide()
     }
 
     handleCheckbox(event, obj1) {
-      console.log(event)
       const {value, checked} = event.target; // get id and checked status
       var selectedData = this.state.selectedData.slice();
-      console.log(obj1)
+      
       // Add to an array if checked and remove - if uncheck
       if (checked) {
         selectedData.push(obj1);
       } else {
         selectedData = selectedData.filter(obj => obj !== obj1);
       }
-      console.log(selectedData)
+
       this.setState({ selectedData: selectedData });
     }
   
     render() {
-      let partners = this.props && this.props.extractObjects.length > 0 ?
-      this.props.extractObjects.map( (obj, i)=>
-        <tr key={`objects-${i}`}>
-        <td >
-          {obj.title}
-        </td>
-        <td >
-          {obj.value}
-        </td>
-        <td>
-          {obj.type}
-        </td>
-        <td>
-          {obj.description}
-        </td>
-      </tr>
-      ) : <tr></tr>;
       return (
         <Modal show={this.props.show}>
             <Modal.Header closeButton>
@@ -73,49 +59,38 @@ export default class ListExtract extends React.Component {
             <Modal.Body>
        
           <Table striped bordered hover>
-           <thead>
-            <tr>
-              <th style={{ width: "1%" }}></th>
-              <th style={{ width: "25%" }}>Título</th>
-              <th >Valor</th>
-              <th className="acao" style={{ width: "15%" }}>Tipo</th>
-              <th className="acao" style={{ width: "35%" }}>Descrição</th>
-            </tr>
-          </thead>
-            <tbody>
-              
+            <thead>
+              <tr>
+                <th style={{ width: "1%" }}></th>
+                <th style={{ width: "25%" }}>Título</th>
+                <th >Valor</th>
+                <th className="acao" style={{ width: "15%" }}>Tipo</th>
+                <th className="acao" style={{ width: "35%" }}>Descrição</th>
+              </tr>
+            </thead>
+            <tbody>   
             {
-                this.props.extractObjects.map((obj, i) => {
-                  return (
-                    <tr key={`objects-${i}`}  className="mb-3">
-                      <td>
-                       <form><input onChange={(event)=>this.handleCheckbox(event, obj)} className="" type="checkbox" checked={this.state.selectedData.includes(obj)} type="checkbox" />
-                       </form>
-                      </td>
-                      <td >
-                        {obj.title}
-                      </td>
-                      <td >
-                        {obj.value}
-                      </td>
-                      <td>
-                        {obj.type}
-                      </td>
-                      <td>
-                        {obj.description}
-                      </td>
-                    </tr>
-
-                  )
-                })
-              }
+              this.props.extractObjects.map((obj, i) => {
+                return (
+                  <tr key={`objects-${i}`}  className="mb-3">
+                    <td><form>
+                        <input onChange={(event)=>this.handleCheckbox(event, obj)} className="" type="checkbox" checked={this.state.selectedData.includes(obj)} type="checkbox"/>
+                      </form>
+                    </td>
+                    <td>{obj.title}</td>
+                    <td >{obj.value}</td>
+                    <td>{obj.type}</td>
+                    <td>{obj.description}</td>
+                  </tr>
+                )})
+            }
             </tbody>
-            </Table> 
-            </Modal.Body>
-            <Modal.Footer>
+          </Table> 
+          </Modal.Body>
+          <Modal.Footer>
             <Button onClick={this.props.onHide}>Fechar</Button>
-              <Button onClick={this.handleSubmit}  bsStyle="primary">Exportar</Button>
-            </Modal.Footer>
+            <Button onClick={this.handleSubmit}  bsStyle="primary">Exportar</Button>
+          </Modal.Footer>
         </Modal>
     )}
 }
